dbdd9dea163e1fcb1937fda73a01c388eca7de9f
Stephaneie Kanali <stephaniekanali0@gmail.com>
2026-04-07 09:05:28 +0300

    I have made changes to the commerce site to return the user info

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
+ 
+function createLoginTracker(userInfo) {
+    let attemptCount = 0;
+    const maxAttempt=3;
+    const passwordAttempt=(password)=> {
+         attemptCount ++;
+        if (password === userInfo.password) {
+            return "Login successful";
+        }
+        else if (attemptCount >= 3) {
+                return "Account locked due to many attempts";}
 
+            else {
+                return "Login failed";
+            }
+        }
+    }
+
+    const credentials = {
+        "Username": "user1",
+        "password": "password123"
+    };
+
+ 
 
 
 module.exports = {
