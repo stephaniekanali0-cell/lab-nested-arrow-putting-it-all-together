77cad115d34f9f2588288ac27ee7eba7ef8f2dae
Stephaneie Kanali <stephaniekanali0@gmail.com>
2026-04-07 13:07:17 +0300

    I have updated the work and gotten the expected results

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
- 
-function createLoginTracker(userInfo) {
-    let attemptCount = 0;
-    const maxAttempt=3;
-    const passwordAttempt=(password)=> {
-         attemptCount ++;
-        if (password === userInfo.password) {
-            return "Login successful";
-        }
-        else if (attemptCount >= 3) {
-                return "Account locked due to many attempts";}
+ //outer function to initialize the login feature
+ const createLoginTracker =(userInfo)=>{ 
+    let attemptCount = 0;  
+    const  maxAttempt =3; 
+    let locked=false;
 
-            else {
-                return "Login failed";
-            }
-        }
+    //inner function to handle the login attempts
+    return(passwordAttempt) =>{
+if(locked){
+    return("Account locked due to many failed attempts")  }
+
+    if(passwordAttempt===userInfo.password){
+        return("login is successful")}
+ 
+    else{
+        attemptCount++ 
+if(attemptCount >=maxAttempt){ 
+    locked=true;
+}
+return`Attempt ${attemptCount} ; login failed`;
+    } 
+     
     }
-
-    const credentials = {
-        "Username": "user1",
-        "password": "password123"
-    };
-
+}
  
-
-
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
+
+
+const user={"username":"user1", "password":"password123"};
+const login=createLoginTracker(user); 
+
+
+console.log(login("skla"));// Attempt one :login failed 
+ 
+console.log(login("password123"))//Attempt two login is successful 
+ 
+console.log(login("kanash")) //login failed
+ 
+console.log(login("suraya123")) //login failed
+ 
+console.log(login("helenitah123")) //Acoount locked due to many attempts
