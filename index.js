3508a01ed402e8903530afdf149d5f9f25bb7a78
Stephaneie Kanali <stephaniekanali0@gmail.com>
2026-04-07 13:23:07 +0300

    Added changes for jest

--- index.js
+++ index.js
@@ -7,17 +7,17 @@
     //inner function to handle the login attempts
     return(passwordAttempt) =>{
 if(locked){
-    return("Account locked due to many failed attempts")  }
+    return("Account locked due to too many failed  login attempts")  }
 
     if(passwordAttempt===userInfo.password){
-        return("login is successful")}
+        return("Login  successful")}
  
     else{
         attemptCount++ 
 if(attemptCount >=maxAttempt){ 
     locked=true;
 }
-return`Attempt ${attemptCount} ; login failed`;
+return`Attempt ${attemptCount} ; Login failed`;
     } 
      
     }
@@ -28,16 +28,22 @@ module.exports = {
 };
 
 
-/*const user={"username":"user1", "password":"password123"};
+const user={"username":"user1", "password":"password123"};
 const login=createLoginTracker(user); 
 
 
-console.log(login("skla"));// Attempt one :login failed 
+console.log(login("skla"));// Attempt one :Login failed 
  
-console.log(login("password123"))//Attempt two login is successful 
+console.log(login("password123"))//Attempt two Login is successful 
  
-console.log(login("kanash")) //login failed
+console.log(login("kanash")) //Login failed
  
-console.log(login("suraya123")) //login failed
+console.log(login("suraya123")) //Login failed
  
-console.log(login("helenitah123")) //Acoount locked due to many attempts*/
+console.log(login("helenitah123")) //Acoount locked due to many attempts
+
+console.log(login("kanash"))
+
+console.log(login("password123"))
+
+
